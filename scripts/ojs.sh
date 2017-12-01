7cac3508525200363694aa4eb144009d7c7a5d72
Katie <katie@flutterflies.net>
2017-12-01 09:52:01 -0500

    Tweak ojs script to use repo at git.uc.edu

--- scripts/ojs.sh
+++ scripts/ojs.sh
@@ -10,17 +10,10 @@ echo "FLUSH PRIVILEGES" | mysql -uroot -pojs
 cd www
 
 # Clone the OJS repository
-git clone -b develop https://github.com/uclibs/ojs .
-./tools/startSubmodulesTRAVIS.sh
-cp config.TEMPLATE.inc.php config.inc.php
+git clone https://git.uc.edu/UCLIBS/ojs3 .
 mkdir ~/files
 chgrp -R www-data cache public ~/files config.inc.php
 chmod -R ug+w cache public ~/files config.inc.php
-
-# Install Composer dependencies
-cd lib/pkp
-curl -sS https://getcomposer.org/installer | php
-php composer.phar update
 
 # Install OJS
 wget -O - --post-data="adminUsername=admin&adminPassword=admin&adminPassword2=admin&adminEmail=ojs@mailinator.com&locale=en_US&additionalLocales[]=en_US&clientCharset=utf-8&connectionCharset=utf8&databaseCharset=utf8&filesDir=%2fhome%2fojs%2ffiles&encryption=sha1&databaseDriver=mysql&databaseHost=localhost&databaseUsername=ojs&databasePassword=ojs&databaseName=ojs&oaiRepositoryId=ojs2.localhost" "http://localhost/ojs/index.php/index/install/install"
